0ea1c23254202efa8715b32973f2721f2e852767
Niccatania <Niccatania6@gmail.com>
2022-05-07 17:10:26 -0700

    update PORT var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs");
 const app = express();
-const PORT = 3001;
+const PORT= process.env.PORT || 3001
 
 var uniqid = require("uniqid");
 
